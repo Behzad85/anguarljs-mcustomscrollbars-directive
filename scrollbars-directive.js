6966bc1212f89f4c2fa0525c1f183b208a61ce56
Behzad85 <behzad85@gmail.com>
2015-08-19 11:31:12 +0430

    Directive to use in your app
    Directive to use in your app

---
+++ scrollbars-directive.js
@@ -0,0 +1,45 @@
+﻿app.directive('scrollBars', function () {
+    return {
+        restrict: 'A',
+        scope: { theme: "@", position: "@",autoHide:"@" },
+        link: function (scope, element, attrs) {
+            if (!scope.autoHide) {
+                scope.autoHide = true;
+            }
+            element.mCustomScrollbar({
+                axis: "y",
+                theme: scope.theme,
+                scrollInertia: 200,
+                autoDraggerLength: true,
+                autoExpandScrollbar: false,
+                autoHideScrollbar: scope.autoHide,
+                scrollbarPosition: scope.position,
+                contentTouchScroll: true,
+                keyboard: { enable: true },
+                mouseWheel: { preventDefault: false },
+                advanced: { autoScrollOnFocus: false }
+            });
+            console.log("Theme : " + scope.theme + " Position : " + scope.position + " AutoHide : " + scope.autoHide);
+        }
+    };
+});
+app.directive('scrollBarsX', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            element.mCustomScrollbar({
+                theme: "light-thin",
+                suppressScrollY: true,
+                scrollInertia: 200,
+                axis: 'x',
+                autoDraggerLength: true,
+                autoExpandScrollbar: true,
+                contentTouchScroll: true,
+                scrollbarPosition: 'inside',
+                keyboard: { enable: true },
+                advanced:{ autoScrollOnFocus: false },
+                mouseWheel: { preventDefault: true }
+            });
+        }
+    };
+});
